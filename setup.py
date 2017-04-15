8b623555dc221a7acb07b8ab34940f48e8bcd308
Manuel Pata <pata@alface.de>
2017-04-15 22:44:14 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cfnipv6subnetgenerator',
+    version='1.0.1',
+    description='CloudFormation Custom Resource for IPv6 Subnet Generator',
+    author='Manuel Pata',
+    author_email='pata@alface.de',
+    license='BSD',
+    keywords='aws cloudformation ipv6',
+    py_modules=['ipv6subnets'],
+    install_requires=["py2-ipaddress"]
+)
